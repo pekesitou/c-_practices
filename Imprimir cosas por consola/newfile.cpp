5f7d72e805e1a2bbc1b11fca06a20e2c0d5a87e1
pekesitou <88730419+pekesitou@users.noreply.github.com>
2022-12-11 18:57:05 +0100

    Create newfile.cpp

---
+++ Imprimir cosas por consola/newfile.cpp
@@ -0,0 +1,18 @@
+// Imprimir cosas en el código \\
+
+// Operadores \\
+  
+  // \n = Salto de linea \\
+  // << endl = Salto de linea \\
+  // \t = Crea una linea horizontal \\
+  // \\ = Inserta un slash invertido \\
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "PUT0 \n" << endl;
+  cout << "Viva España jdr \t";
+  cout << "Prueba  \\";
+  return 0;
+}
