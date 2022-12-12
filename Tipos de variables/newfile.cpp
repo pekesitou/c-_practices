efa6a71aaecd4a0d1bcdf0f09192c0fd400405ab
pekesitou <88730419+pekesitou@users.noreply.github.com>
2022-12-12 16:08:50 +0100

    Create newfile.cpp

---
+++ Tipos de variables/newfile.cpp
@@ -0,0 +1,37 @@
+// Tipos de variables \\
+    
+    // int: Almacena números sin decimales \\
+    // double: Almacena números con decimales \\
+    // char: Guarda solo un caracter. Por ej: char x = x \\
+    // string: Almacena un texto \\
+    // bool: Variable booleana. Solo almacena true o false
+    
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int sindec = 34;
+    double condec = 34.34;
+    char caracter = 'A';
+    string txt = "Hola mundo!";
+    bool tr = true;
+    bool fl = false;
+    
+    
+    cout << sindec;
+        cout << "\n";
+    cout << condec;
+        cout << "\n";
+    cout << caracter;
+        cout << "\n";
+    cout << txt;
+        cout << "\n";
+    cout << tr;
+        cout << "\n";
+    cout << fl;
+        cout << "\n";
+    
+}
