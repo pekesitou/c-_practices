0670dc27991b4bf73a41209471f456a6c30f7247
pekesitou <88730419+pekesitou@users.noreply.github.com>
2022-12-12 16:10:30 +0100

    Create estructura.cpp

---
+++ Estructura del código/estructura.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "Estructura del código\n";
+        cout << "------------------------------------\n\n";
+    cout << "#include <iostream>\n\n";
+    cout << "using namespace std\n\n";
+        cout << "\n";
+    cout << "\t int main() {\n\n";
+    cout << "\t\t string text = Hola!\n\n";
+    cout << "\t }";
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    
+}
